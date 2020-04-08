48aa9dfaf72d1d4639318c205df07f6e91a0f88a
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2020-04-08 11:36:34 +0200

    Add more tests

--- request/node-tests/src/util.ts
+++ request/node-tests/src/util.ts
@@ -1,18 +1,18 @@
 import baseTest, { TestInterface } from "ava"; // eslint-disable-line
-import http, { IncomingMessage, ServerResponse } from "http";
+import http, { IncomingMessage, RequestListener, ServerResponse } from "http";
 import https from "https";
 import net, { AddressInfo } from "net";
 
 export const test = baseTest as TestInterface<Context>;
 
-test.beforeEach(async (t) => {
+test.beforeEach((t) => {
   t.context = {
-    server: await startServer(),
+    server: new TestServer(),
   };
 });
 
-test.afterEach(async (t) => {
-  await t.context.server.close();
+test.afterEach(async ({ context }) => {
+  await context.server.close();
 });
 
 export interface Context {
@@ -31,63 +31,57 @@ export interface Route {
 }
 
 export class Router {
-  readonly #routes: Route[] = [];
+  readonly routes: Route[] = [];
 
   add(method: string, url: string, listener: AsyncRequestListener): this {
-    this.#routes.push({ method, url, listener });
+    this.routes.push({ method, url, listener });
     return this;
   }
 
-  handle(req: IncomingMessage, res: ServerResponse): void {
-    const { method, url } = req;
-    for (const route of this.#routes) {
-      if (route.method === method && route.url === url) {
-        const result = route.listener(req, res);
-        if (result instanceof Promise) {
-          result.catch((err) => {
-            res.statusCode = 500;
-            res.setHeader("Content-Type", "text/plain");
-            res.end(String(err));
-          });
+  listen(): RequestListener {
+    return (req: IncomingMessage, res: ServerResponse): void => {
+      const { method, url } = req;
+      for (const route of this.routes) {
+        if (route.method === method && route.url === url) {
+          const result = route.listener(req, res);
+          if (result instanceof Promise) {
+            result.catch((err) => {
+              this.status500(req, res, err);
+            });
+          }
+          return;
         }
-        return;
       }
-    }
-    this.notFound(req, res);
+      this.status404(req, res);
+    };
   }
 
-  notFound(req: IncomingMessage, res: ServerResponse): void {
+  status404(req: IncomingMessage, res: ServerResponse): void {
     const { method, url } = req;
     res.statusCode = 404;
     res.setHeader("Content-Type", "text/plain");
-    res.end(`Resource not found: ${method}:${url}`);
+    res.end(`Not Found: ${method}:${url}`);
+  }
+
+  status500(req: IncomingMessage, res: ServerResponse, err: Error): void {
+    const { method, url } = req;
+    res.statusCode = 500;
+    res.setHeader("Content-Type", "text/plain");
+    res.end(`Internal Server Error: ${method}:${url}: ${String(err)}`);
   }
 }
 
 export class TestServer {
+  readonly server: net.Server;
   readonly baseUrl: string;
+  readonly router: Router;
 
-  constructor(readonly server: net.Server, readonly router: Router) {
-    const { address, family, port } = this.server.address() as AddressInfo;
-    const protocol = (() => {
-      if (server instanceof http.Server) {
-        return "http";
-      }
-      if (server instanceof https.Server) {
-        return "https";
-      }
-      throw new Error(); // Unreachable.
-    })();
-    switch (family) {
-      case "IPv4":
-        this.baseUrl = `${protocol}://${address}:${port}/`;
-        break;
-      case "IPv6":
-        this.baseUrl = `${protocol}://[${address}]:${port}/`;
-        break;
-      default:
-        throw new Error(); // Unreachable.
-    }
+  constructor() {
+    const router = new Router();
+    const server = http.createServer(router.listen());
+    this.server = server;
+    this.baseUrl = baseUrl(server);
+    this.router = router;
   }
 
   url(url: string): string {
@@ -112,17 +106,33 @@ export class TestServer {
   }
 }
 
-export function startServer(): Promise<TestServer> {
-  const router = new Router();
-  return new Promise<TestServer>((resolve, reject) => {
-    const server = http.createServer((req, res) => {
-      router.handle(req, res);
-    });
-    try {
-      server.listen();
-      resolve(new TestServer(server, router));
-    } catch (err) {
-      reject(err);
+function baseUrl(server: net.Server): string {
+  let a = server.address() as AddressInfo;
+  if (a == null) {
+    server.listen();
+    a = server.address() as AddressInfo;
+    if (a == null) {
+      throw new Error(); // Unreachable.
     }
-  });
+  }
+  const { address, family, port } = a;
+  const protocol = serverProtocol(server);
+  switch (family) {
+    case "IPv4":
+      return `${protocol}://${address}:${port}/`;
+    case "IPv6":
+      return `${protocol}://[${address}]:${port}/`;
+    default:
+      throw new Error(); // Unreachable.
+  }
+}
+
+function serverProtocol(server: net.Server): string {
+  if (server instanceof http.Server) {
+    return "http";
+  }
+  if (server instanceof https.Server) {
+    return "https";
+  }
+  throw new Error(); // Unreachable.
 }
