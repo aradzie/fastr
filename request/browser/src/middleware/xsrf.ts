60285af0321547cbb7a29e64da6040911ddc2895
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2020-04-22 16:18:01 +0200

    Update the XSRF middleware

--- request/browser/src/middleware/xsrf.ts
+++ request/browser/src/middleware/xsrf.ts
@@ -1,8 +1,18 @@
+import { Cookie, Headers } from "@webfx-http/headers";
 import type { Adapter, HttpRequest, HttpResponse, Middleware } from "../types";
 
 export interface XsrfOptions {
+  /**
+   * The default value is `"XSRF-TOKEN"`.
+   */
   readonly cookieName?: string;
+  /**
+   * The default value is `"X-XSRF-TOKEN"`.
+   */
   readonly headerName?: string;
+  /**
+   * Obtains the XSRF token to use with a request.
+   */
   readonly cookieReader?: (cookieName: string) => string | null;
 }
 
@@ -19,11 +29,32 @@ export function xsrf(options: XsrfOptions = {}): Middleware {
     request: HttpRequest,
     adapter: Adapter,
   ): Promise<HttpResponse> => {
-    // TODO Implement me.
-    return adapter(request);
+    const { url, method } = request;
+    const urlLc = url.toLowerCase();
+    const methodUc = (method ?? "GET").toUpperCase();
+    if (
+      methodUc === "GET" ||
+      methodUc === "HEAD" ||
+      urlLc.startsWith("http://") ||
+      urlLc.startsWith("https://")
+    ) {
+      return adapter(request);
+    }
+    const headers = new Headers(request.headers);
+    if (headers.has(headerName)) {
+      return adapter(request);
+    }
+    const token = cookieReader(cookieName);
+    if (token == null) {
+      return adapter(request);
+    }
+    return adapter({
+      ...request,
+      headers: headers.set(headerName, token),
+    });
   };
 }
 
 function documentCookieReader(cookieName: string): string | null {
-  return null;
+  return Cookie.parse(window.document.cookie).get(cookieName) || null;
 }
