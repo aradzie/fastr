4e21d3d61bdeffce8129fee49e89babd4cdc0b27
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2020-04-23 13:46:37 +0200

    Improve request body handling in the node request library

--- request/node/src/body/type.test.ts
+++ request/node/src/body/type.test.ts
@@ -1,45 +1,105 @@
 import test from "ava";
+import { Readable } from "stream";
+import { Streamable } from "./streamable";
 import { guessContentType } from "./type";
 
-test("use custom type", (t) => {
-  t.is(guessContentType("text", "foo/bar")[1], "foo/bar");
-});
-
 test("guess text type", (t) => {
-  t.is(guessContentType("text", null)[1], "text/plain");
+  const body = "text";
+  t.deepEqual(
+    guessContentType(body, null), //
+    [body, "text/plain; charset=UTF-8"],
+  );
+  t.deepEqual(
+    guessContentType(body, "foo/bar"), //
+    [body, "foo/bar"],
+  );
 });
 
 test("guess buffer type", (t) => {
-  t.is(guessContentType(Buffer.alloc(0), null)[1], "application/octet-stream");
+  const body = Buffer.alloc(0);
+  t.deepEqual(
+    guessContentType(body, null), //
+    [body, "application/octet-stream"],
+  );
+  t.deepEqual(
+    guessContentType(body, "foo/bar"), //
+    [body, "foo/bar"],
+  );
+});
+
+test("guess readable type", (t) => {
+  const body = Readable.from([]);
+  t.deepEqual(
+    guessContentType(body, null), //
+    [body, "application/octet-stream"],
+  );
+  t.deepEqual(
+    guessContentType(body, "foo/bar"), //
+    [body, "foo/bar"],
+  );
+});
+
+test("guess streamable type", (t) => {
+  const body = new (class extends Streamable {
+    length(): number | null {
+      return 0;
+    }
+
+    open(): Readable {
+      return Readable.from([]);
+    }
+  })();
+  t.deepEqual(
+    guessContentType(body, null), //
+    [body, "application/octet-stream"],
+  );
+  t.deepEqual(
+    guessContentType(body, "foo/bar"), //
+    [body, "foo/bar"],
+  );
 });
 
 test("guess form type", (t) => {
-  t.is(
-    guessContentType(new URLSearchParams(), null)[1],
-    "application/x-www-form-urlencoded",
+  const body = new URLSearchParams([["a", "1"]]);
+  t.deepEqual(
+    guessContentType(body, null), //
+    ["a=1", "application/x-www-form-urlencoded; charset=UTF-8"],
   );
 });
 
 test("guess json type", (t) => {
+  const plainObject = { a: 1 };
+  const nonPlainObject = new (class Dummy {
+    ignored = 0;
+
+    toJSON(): unknown {
+      return plainObject;
+    }
+  })();
+
   // Try plain object.
-  t.deepEqual(guessContentType({ a: 1 }, null), [
+  t.deepEqual(guessContentType(plainObject, null), [
     '{"a":1}',
-    "application/json",
+    "application/json; charset=UTF-8",
+  ]);
+
+  // Try plain object with custom type.
+  t.deepEqual(guessContentType(plainObject, "application/foobar+json"), [
+    '{"a":1}',
+    "application/foobar+json",
   ]);
 
   // Try non-plain object with the toJSON method.
-  t.deepEqual(
-    guessContentType(
-      new (class Dummy {
-        ignored = 0;
-        toJSON(): unknown {
-          return { a: 1 };
-        }
-      })(),
-      null,
-    ),
-    ['{"a":1}', "application/json"],
-  );
+  t.deepEqual(guessContentType(nonPlainObject, null), [
+    '{"a":1}',
+    "application/json; charset=UTF-8",
+  ]);
+
+  // Try non-plain object with the toJSON method with custom type.
+  t.deepEqual(guessContentType(nonPlainObject, "application/foobar+json"), [
+    '{"a":1}',
+    "application/foobar+json",
+  ]);
 });
 
 test("reject invalid type", (t) => {
--- request/node/src/body/type.ts
+++ request/node/src/body/type.ts
@@ -3,27 +3,32 @@ import { Readable } from "stream";
 import { BodyDataType } from "../types";
 import { Streamable } from "./streamable";
 
+const S_TEXT = "text/plain; charset=UTF-8";
+const S_FORM = "application/x-www-form-urlencoded; charset=UTF-8";
+const S_JSON = "application/json; charset=UTF-8";
+const S_BINARY = "application/octet-stream";
+
 export function guessContentType(
   body: BodyDataType | URLSearchParams | object,
   contentType: string | null,
 ): [BodyDataType, string] {
   if (typeof body === "string") {
-    return [body, contentType ?? "text/plain"];
+    return [body, contentType ?? S_TEXT];
   }
   if (Buffer.isBuffer(body)) {
-    return [body, contentType ?? "application/octet-stream"];
+    return [body, contentType ?? S_BINARY];
   }
   if (body instanceof Readable) {
-    return [body, contentType ?? "application/octet-stream"];
+    return [body, contentType ?? S_BINARY];
   }
   if (body instanceof Streamable) {
-    return [body, contentType ?? "application/octet-stream"];
+    return [body, contentType ?? S_BINARY];
   }
   if (body instanceof URLSearchParams) {
-    return [String(body), contentType ?? "application/x-www-form-urlencoded"];
+    return [String(body), contentType ?? S_FORM];
   }
   if (isJSON(body)) {
-    return [JSON.stringify(body), contentType ?? "application/json"];
+    return [JSON.stringify(body), contentType ?? S_JSON];
   }
   throw new TypeError("Invalid body object.");
 }
