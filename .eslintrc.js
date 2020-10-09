69d2136ad7d1a3fc3f345dee6d4339d1f7391c1b
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2020-10-09 11:52:46 +0200

    Simplify eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,8 @@ module.exports = {
     es2020: true,
     node: true,
   },
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: "module",
-  },
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:node/recommended",
     "plugin:ava/recommended",
